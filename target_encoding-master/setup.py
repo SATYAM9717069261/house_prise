78a683cade56555a8647844fdee0aa8cbf704ae6
Satyam Mandal <satyam.mandal.77@gmail.com>
2020-11-10 15:30:06 +0530

    Update setup.py

--- target_encoding-master/setup.py
+++ target_encoding-master/setup.py
@@ -16,7 +16,7 @@ setup(
         'numpy==1.16.2',
         'scikit-learn==0.20.3',
     ],
-    url="https://github.com/KirillTushin/target_encoding",
+    url="https://github.com/SATYAM9717069261/target_encoding",
     test_suite='tests',
     classifiers=[
         "Programming Language :: Python :: 3",
